7da8c58fd9cd08695d93f2fa760262116eab3473
SaMu-mo <menesesorozcosamuel@gmail.com>
2026-05-09 20:31:47 -0500

    Creo el repo

---
+++ examen_samuel_meneses/src/main/java/com/krakedev/examen_samuel_meneses/ExamenSamuelMenesesApplication.java
@@ -0,0 +1,13 @@
+package com.krakedev.examen_samuel_meneses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenSamuelMenesesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenSamuelMenesesApplication.class, args);
+	}
+
+}
---
+++ examen_samuel_meneses/src/test/java/com/krakedev/examen_samuel_meneses/ExamenSamuelMenesesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krakedev.examen_samuel_meneses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenSamuelMenesesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
