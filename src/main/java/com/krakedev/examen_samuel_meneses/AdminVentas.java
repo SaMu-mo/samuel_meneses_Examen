2aa13f08eb7ca8aa93dd1320ec9f65db3d203ad2
SaMu-mo <menesesorozcosamuel@gmail.com>
2026-05-09 21:02:58 -0500

    Parte 2 del examen acabada

---
+++ src/main/java/com/krakedev/examen_samuel_meneses/AdminVentas.java
@@ -0,0 +1,21 @@
+package com.krakedev.examen_samuel_meneses;
+
+import java.util.ArrayList;
+
+public class AdminVentas {
+
+    private ArrayList<Vendedor> vendedores = new ArrayList<>();
+
+    public void agregar(Vendedor vendedor) {
+        vendedores.add(vendedor);
+    }
+
+    public Double calcularSueldo(String cedula) {
+        for (Vendedor v : vendedores) {
+            if (v.getCedula().equals(cedula)) {
+                return v.calcularSueldo();
+            }
+        }
+        return null;
+    }
+}
